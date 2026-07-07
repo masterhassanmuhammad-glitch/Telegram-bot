6db29cb44c432aba05f2acf388c5c00544032141
masterhassanmuhammad-glitch <masterhassanmuhammad@gmail.com>
2026-07-07 17:01:00 +0200

    Update config.py to

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
 import os
-import logging
 
-# إعدادات تسجيل الأحداث (Logs)
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
-)
+# ==========================================
+# Telegram
+# ==========================================
 
-# توكن البوت من BotFather
-API_TOKEN = os.getenv("API_TOKEN")
-if not API_TOKEN:
-    raise RuntimeError("API_TOKEN is missing.")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# رابط قاعدة البيانات
+# ==========================================
+# Database
+# ==========================================
+
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is missing.")
 
-# معرف مالك البوت (الأدمن الأساسي)
-OWNER_ID = int(os.getenv("OWNER_ID", "8203001172"))
+# ==========================================
+# Render
+# ==========================================
 
-# رابط الويب هوك الخارجي (مهم لمنصة Render)
 RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL")
 
-# المنفذ الخاص بالسيرفر
-PORT = int(os.getenv("PORT", "5000"))
+PORT = int(os.getenv("PORT", 10000))
+
+# ==========================================
+# Debug
+# ==========================================
+
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
