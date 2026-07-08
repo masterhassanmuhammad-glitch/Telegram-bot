4b15884c6edc5b4030501c9d8aeae68017c21ea4
masterhassanmuhammad-glitch <masterhassanmuhammad@gmail.com>
2026-07-08 19:45:38 +0200

    Update application.py

--- core/application.py
+++ core/application.py
@@ -12,7 +12,9 @@ from handlers.admin import register as register_admin
 register_admin(application)
 from handlers.start import register as register_start
 from handlers.callback import register as register_callback
+from handlers.admin_add_menu import register as register_admin_add_menu
 
+register_admin_add_menu(application)
 
 application = (
     Application.builder()
