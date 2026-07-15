877fe14dce4afc43f2914167b35c7be9ce67da9f
masterhassanmuhammad-glitch <masterhassanmuhammad@gmail.com>
2026-07-15 22:56:18 +0200

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,3 +1,4 @@
+from .helpers import active_menus, send_files_and_recreate_menu
 from config import bot, OWNER_ID, ADMIN_IDS
 from database import execute_query, set_user_state, clear_user_state
 from keyboards import make_main_menu_markup, make_sub_menu_markup
