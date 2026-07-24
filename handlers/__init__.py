dad2fe0b20fae889402e294ade27d6723a422552
masterhassanmuhammad-glitch <masterhassanmuhammad@gmail.com>
2026-07-24 20:40:27 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,12 @@
 from .admin import register_admin_handlers  
+from .user import register_user_handlers
 from .owner import register_owner_handlers
-from .user import register_user_handlers      
 from admin_logs import register_logs_handlers
 
 def init_handlers():
     # تسجيل معالجات الإدارة والمستخدمين والمالك والسجلات
     register_admin_handlers() 
-    register_owner_handlers()
     register_user_handlers()
+    register_owner_handlers()
     register_logs_handlers()
     
